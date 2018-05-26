32284e8bfb1ccf3449a7b8c840b8aeccc958145b
Claas Lisowski <claas.lisowski@kreditech.com>
2018-05-26 17:54:48 +0200

    Add suitecrm referer code again

--- fpm/app/bootstrap/suitecrm-init.sh
+++ fpm/app/bootstrap/suitecrm-init.sh
@@ -7,6 +7,16 @@ echo "##########################################################################
 
 chown -R www-data:www-data . && chmod -R 755 . && chmod -R 775 custom modules themes data upload
 
+if [ -n "${HTTP_REFERER}" ]; then
+  ref_number=0
+  for referer in ${HTTP_REFERER//,/ }; do
+    if ! grep -Eq http_referer.*${referer} ${CONFIG_OVERRIDE_FILE} ; then
+      printf "\n\$sugar_config['http_referer']['list'][${ref_number}] = '${referer}';" >> ${CONFIG_OVERRIDE_FILE}
+    fi
+    ref_number=$((ref_number+1))
+  done
+fi
+
 exec busybox crond -f -l 0 -L /dev/stdout &
 
 exec "$@"
